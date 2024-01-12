521b3822556fbb145cc85f34ed0851fcc6bcbbff
Your Name <pattr@PETARDA>
2024-01-12 10:03:38 +0100

    Add project files.

---
+++ PictureTask/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PictureTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pcbPicture = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClearPicture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 173);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Picture";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pcbPicture
+            // 
+            this.pcbPicture.Location = new System.Drawing.Point(328, 98);
+            this.pcbPicture.Name = "pcbPicture";
+            this.pcbPicture.Size = new System.Drawing.Size(452, 340);
+            this.pcbPicture.TabIndex = 1;
+            this.pcbPicture.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.Location = new System.Drawing.Point(678, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(102, 50);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClearPicture
+            // 
+            this.btnClearPicture.BackColor = System.Drawing.Color.LightCoral;
+            this.btnClearPicture.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnClearPicture.Location = new System.Drawing.Point(292, 97);
+            this.btnClearPicture.Name = "btnClearPicture";
+            this.btnClearPicture.Size = new System.Drawing.Size(35, 35);
+            this.btnClearPicture.TabIndex = 3;
+            this.btnClearPicture.Text = "X";
+            this.btnClearPicture.UseVisualStyleBackColor = false;
+            this.btnClearPicture.Click += new System.EventHandler(this.btnClearPicture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClearPicture);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.pcbPicture);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Picture Show";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pcbPicture;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClearPicture;
+    }
+}
+
---
+++ PictureTask/Form1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PictureTask
+{
+    public partial class Form1 : Form
+    {
+        string folderName = "Pictures";
+        string fileName = "Readme.txt";
+        bool firstMessage = true;
+        bool createFolder = true;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnClearPicture.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pathImage = Path.Combine(Directory.GetCurrentDirectory() + @"\..\..\", folderName);
+            if (createFolder)
+            {
+                Directory.CreateDirectory(pathImage);
+                File.Create(Path.GetFullPath(pathImage) + $@"\{fileName}");
+                createFolder = false;
+            }
+
+            using (var ofd = new OpenFileDialog())
+            {
+                if (firstMessage)
+                {
+                    MessageBox.Show($"Created new folder: {folderName}\n\nAdd images to this folder\n\nLocalization folder: \n{Path.GetFullPath(pathImage)}", "Add Picture", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    firstMessage = false;
+                }
+
+                ofd.InitialDirectory = Path.GetFullPath(pathImage);
+                ofd.RestoreDirectory = true;
+                ofd.Filter = "JPG only (*.jpg|*.jpg";
+                MessageBox.Show("Please select image from the folder.", "Select Picture",MessageBoxButtons.OK);
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedImage = ofd.FileName;
+                    pcbPicture.ImageLocation = selectedImage;
+                    pcbPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+                    btnClearPicture.Visible = true;
+                }
+            }
+
+        }
+
+        private void btnClearPicture_Click(object sender, EventArgs e)
+        {
+            pcbPicture.Image = null;
+            btnClearPicture.Visible = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
